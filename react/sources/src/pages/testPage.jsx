6ee3a13b74be8b42f0cf11748a19a44ef1427312
udongmandu <qjawns10000@naver.com>
2024-03-18 10:41:23 +0900

    react 파일 초기화

---
+++ react/sources/src/pages/testPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function testPage() {
+  return <div className="text-2xl font-black">테스트 페이지</div>;
+}
--- react/sources/src/template/Consist.jsx
+++ react/sources/src/template/Consist.jsx
@@ -1,55 +1,14 @@
 import React from "react";
-import "../style/index.css";
-import SideBar from "./SideBar";
-import Topbar from "./Topbar";
-import MainPage from "../pages/student/main";
-import LoginPage from "../pages/login/signIn";
-import RegisterPage from "../pages/register/register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StudentEdit from "../pages/student/student-edit/studentEdit";
-import MainPageTeacher from "../pages/teacher/teacher_Check";
-import ScheduleStudnet from "../pages/student/schedule_student";
-import AttandanceStudnet from "../pages/student/attendance_student";
-import AttandanceTeacher from "../pages/teacher/attendeace_teacher";
-import StudentAdd from "../pages/student/student-add/studentAdd";
+import TestPage from "../pages/testPage";
 
 export default function Consist() {
   return (
     <BrowserRouter>
       <div className="w-full h-full flex flex-col bg-[#FAFBFE]">
-        <Topbar />
-        <div className="flex h-fit bg-[#FAFBFE]">
-          <SideBar />
-          <div className="w-full">
-            <Routes>
-              {/* 로그인 , 회원가입 페이지 */}
-              <Route path="/sign-in" element={<LoginPage />} />
-              <Route path="/register-page" element={<RegisterPage />} />
-              {/* --- 학생 ---  */}
-              {/* 학생관리 페이지 */}
-              <Route path="/student" element={<MainPage />} />
-              <Route path="/schedule_student" element={<ScheduleStudnet />} />
-              <Route
-                path="/attendance-student"
-                element={<AttandanceStudnet />}
-              />
-              {/* 학생정보 수정 페이지 */}
-              <Route
-                path="/student-edit/:studentID"
-                element={<StudentEdit />}
-              />
-              {/* 학생 추가 페이지 */}
-              <Route path="/student-add" element={<StudentAdd />} />
-              {/* --- 교직원 ---  */}
-              {/* 교직원 관리 페이지 */}
-              <Route path="/teacher" element={<MainPageTeacher />} />
-              <Route
-                path="/attendance-teacher"
-                element={<AttandanceTeacher />}
-              />
-            </Routes>
-          </div>
-        </div>
+        <Routes>
+          <Route path="/" element={<TestPage />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
